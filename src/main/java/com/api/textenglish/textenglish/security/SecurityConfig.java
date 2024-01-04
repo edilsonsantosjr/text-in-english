7ccb895538e21b855159ebba352da8e0508d8384
Edilson Santos <edilson0982021@gmail.com>
2024-01-04 18:39:40 -0300

    Only allowing admin to register new users

--- src/main/java/com/api/textenglish/textenglish/security/SecurityConfig.java
+++ src/main/java/com/api/textenglish/textenglish/security/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((request) -> request
-                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/texts").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.GET, "/texts/**").permitAll()
